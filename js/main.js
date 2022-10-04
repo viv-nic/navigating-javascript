267e94262f1123d2b59658d6541c75a7c2959efc
“viv-nic” <vivian1005@hotmail.com>
2022-10-04 19:24:49 +1100

    JS-Lesson 1

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+// console.log('hi');
+
+// function codingBoss() {
+//     alert("yo yo yo!");
+//     document.querySelector('.hand').style.backgroundColor = "hotpink";
+// }
+
+// codingBoss();
+
+let firstPrize = 299;
+const companyName = "CLG";
+let greeting = "Hello";
+let morning = true;
+ 
+console.log(morning);
+console.log(firstPrize);
+console.clear();
+
+let firstWinner = "Grace";
+// console.log("The winner is " + firstWinner);
+//console.log(`The winner is still ${firstWinner}`);
+
+let announcementWinner = `winner is still ${firstWinner}`;
+
+let result = announcementWinner.toUpperCase();
+console.log(result);
